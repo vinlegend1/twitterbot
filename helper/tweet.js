0cf93eba22ada313cd3eb8e52239d6f02ea0a38a
Vincent Hong <vinlegend1@gmail.com>
2020-09-28 16:18:03 +0800

    first time writing clean code

--- helper/tweet.js
+++ helper/tweet.js
@@ -1,5 +1,18 @@
-module.exports = function tweetIt(T, tweetText) {
-    T.post('statuses/update', { status: tweetText }, (err, data, response) => {
-        console.log(data);
-    });
+module.exports = {
+    tweetIt: function tweetIt(T, tweetText) {
+        T.post('statuses/update', { status: tweetText }, (err, data, response) => {
+            console.log(data);
+        });
+    },
+    tweetCopier: function tweetCopier(T, trackArr) {
+        const stream = T.stream('statuses/filter', { track: trackArr })
+
+        stream.on('tweet', tweet => {
+            const random = Math.floor(Math.random() * 1000)
+            const newTweet = tweet ? `${tweet.text} - @${tweet.user.screen_name} P.S. this is a bot. I still have to figure out how to retweet.` : random;
+            T.post('statuses/update', { status: newTweet }, (err, data, response) => {
+                console.log(data);
+            });
+        })
+    }
 }
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const Twit = require('twit');
-const tweetIt = require('./helper/tweet');
+const { tweetIt, tweetCopier } = require('./helper/tweet');
 
 // config.js is "git-ignored" and all it has is my api and access token things
 const { consumer_key, consumer_secret, access_token, access_token_secret } = require('./config')
@@ -16,19 +16,4 @@ const T = new Twit({
 // let random = Math.floor(Math.random() * 1000);
 // setInterval(() => tweetIt(T, "Hello from Node " + random), time_break);
 
-// const stream = T.stream('user', { track: ['python'] });
-
-// stream.on('statuses/filter', (tweet) => {
-//     // const name = eventMsg.source.name;
-//     // const screenName = eventMsg.source.screen_name;
-//     // tweetIt(T, ``)
-//     console.log(tweet);
-// })
-const stream = T.stream('statuses/filter', { track: ['thisissomethingrandom'] })
-
-stream.on('tweet', tweet => {
-    let random = Math.floor(Math.random() * 1000)
-    const newTweet = tweet ? `${tweet.text} - @${tweet.user.screen_name} P.S. this is a bot. I still have to figure out how to retweet.` : random;
-    // console.log(tweet.user.screen_name);
-    tweetIt(T, newTweet);
-})
+tweetCopier(T, ['java']);
