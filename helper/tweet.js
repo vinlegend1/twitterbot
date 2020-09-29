a7587cab98e6ebd058c8b49e04e92cd13cf141c3
Vincent Hong <vinlegend1@gmail.com>
2020-09-29 11:43:00 +0800

    reorganizing things

--- helper/tweet.js
+++ helper/tweet.js
@@ -14,5 +14,33 @@ module.exports = {
                 console.log(data);
             });
         })
+    },
+    tweetWithImage: function tweetWithImage(T, b64contentImage, tweetMessage, altOfImage) {
+
+        T.post('media/upload', { media_data: b64contentImage }, (err, data, response) => {
+
+            // now we can assign alt text to the media, for use by screen readers and
+            // other text-based presentations and interpreters
+
+            const mediaIdStr = data.media_id_string
+            const altText = altOfImage;
+            const meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+          
+            T.post('media/metadata/create', meta_params, (err, data, response) => {
+              if (!err) {
+                // now we can reference the media and post a tweet (media will attach to the tweet)
+                const params = { status: tweetMessage, media_ids: [mediaIdStr] }
+          
+                T.post('statuses/update', params, (err, data, response) => {
+                  console.log(data)
+                })
+
+              } else {
+                  console.log("crap");
+              }
+            });
+
+        });
+
     }
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const Twit = require('twit');
-const { tweetIt, tweetCopier } = require('./helper/tweet');
+const { tweetIt, tweetCopier, tweetWithImage } = require('./helper/tweet');
+const fs = require('fs');
 
 // config.js is "git-ignored" and all it has is my api and access token things
 const { consumer_key, consumer_secret, access_token, access_token_secret } = require('./config')
@@ -12,8 +13,18 @@ const T = new Twit({
     access_token_secret
 });
 
+// tweetIt Implementation with setInterval()
+
 // const time_break = 1000 * 60;
 // let random = Math.floor(Math.random() * 1000);
 // setInterval(() => tweetIt(T, "Hello from Node " + random), time_break);
 
-tweetCopier(T, ['java']);
+// tweetCopier Implementation
+
+// tweetCopier(T, ['java']);
+
+// tweetWithImage Implementation
+
+// const b64content = fs.readFileSync('./images/khanacademy.jpg', { encoding: 'base64' })
+// tweetWithImage(T, b64content, "Hello this is annother image", "Khan Academy is awesome");
+
